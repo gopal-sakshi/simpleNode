8548f23e0168484c259519b28dec887618d204ec
gopal-sakshi <gopal612@gmail.com>
2022-03-31 10:50:54 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const PORT = process.env.PORT || 3055;
+
+const server = http.createServer(async (req, res) => {
+    const bodyToSend = "gopal23"
+    if(req.url === '/route1', req.method === "GET") {
+        res.writeHead(200, {"Content-Type": "application/json"});
+        // res.writeHead(200, {'Content-Length': Buffer.byteLength(bodyToSend), 'Content-Type': 'text/plain'});
+        res.write(`Hi, you've hit route1 with ${req.method} method`);
+        res.end();
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`server23 started on port ${PORT}`);
+})
+
+// https://www.section.io/engineering-education/a-raw-nodejs-rest-api-without-frameworks-such-as-express/
---
+++ readMe.txt
@@ -0,0 +1,16 @@
+Node.js
+- popular JavaScript framework
+- Its not just used for web servers... its also used in desktop & mobile
+    when used in desktop/mobile, we dont need express module
+
+use Electron and build desktop apps
+- electron internally uses nodeJs
+- so, for building desktop apps, we employ "nodeJs powered Electron" where we dont need express module
+
+Anyway, we need to know how to build web server using "vanilla NodeJs"
+- no express
+- no other middleware
+- no npm, package.json, no stuff
+- we only use HTTP MODULE
+---------------------------------------------------------------------------------------------------------
+
