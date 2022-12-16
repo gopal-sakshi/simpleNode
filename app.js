50f3fbcd9cd4b1a7814c0bb6e3393c01cbe25f8f
gopal-sakshi <gopal612@gmail.com>
2022-12-16 14:37:02 +0530

    added methods simpleNode

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const http = require("http");
 const PORT = process.env.PORT || 3058;
 
-const server = http.createServer(async (req, res) => {
-    const bodyToSend = "gopal23"
-    if(req.url === '/route1' && req.method === "GET") {
-        res.writeHead(200, {"Content-Type": "application/json"});
-        // res.writeHead(200, {'Content-Length': Buffer.byteLength(bodyToSend), 'Content-Type': 'text/plain'});
-        res.write(`Hi, you've hit route1 with ${req.method} method`);
-        res.end();
-    }
-    if(req.url === '/route2') {
-        const res23 = await getRequestBody(req);
-        console.log(res23);
+/*************************** Route Callbacks ******************************/
+const route1Cb = (req, res) => { 
+    res.writeHead(200, {"Content-Type": "application/json"});
+    // res.writeHead(200, {'Content-Length': Buffer.byteLength(bodyToSend), 'Content-Type': 'text/plain'});
+    res.write(`Hi, you've hit route1 with ${req.method} method`);
+    res.end();
+}
+
+const route2Cb = async (req, res) => {
+    const res23 = await getRequestBody(req);
+    console.log(res23);
+    res.write(JSON.stringify(res23));
+    res.end();
+}
+
+const route3Cb = (req, res) => {
+    var res23 = '';
+    // req.on('data', (requestChunk) => { console.log(requestChunk); res23 = res23 + requestChunk});
+    req.on('data', requestChunk => res23 = res23 + requestChunk);
+    req.on('end', () => {
         res.write(JSON.stringify(res23));
         res.end();
-    }
-});
+    })    
+}
+/*************************************************************************** */
+
+
+
+
+
+
+
+/******************************* UTILITY Functions ***************************** */
+const reqCbFn = async (req, res) => {
+    var cbFn = findCbFromReqUrl(req.url);
+    return await cbFn(req, res);
+}
+const findCbFromReqUrl = (url) => (routes.find(route => route.path == url)).cbFn
 
 function getRequestBody(reqObj) {
     return new Promise((resolve, reject) => {
@@ -29,9 +52,25 @@ function getRequestBody(reqObj) {
     });
 }
 
+const routes = [
+    { path: "path23", method: "method23", cbFn: {} },
+    { path: "/route1", method: "GET", cbFn: route1Cb },
+    { path: "/route2", method: "PUT", cbFn: route2Cb },
+    { path: "/route3", method: "PUT", cbFn: route3Cb },
+];
+/*************************************************************************** */
 
+
+
+
+    
+    
+/******************************* SERVER SETUP *******************************/
+const server = http.createServer();
+server.on('request', reqCbFn);
 server.listen(PORT, () => {
     console.log(`server23 started on port ${PORT}`);
 })
+/*************************************************************************** */
 
 // https://www.section.io/engineering-education/a-raw-nodejs-rest-api-without-frameworks-such-as-express/
