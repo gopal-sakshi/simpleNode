49ef97e15e48a4126a443da71983baef637d83e0
gopal-sakshi <gopal612@gmail.com>
2022-12-16 11:01:30 +0530

    process request body with simpleNode

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ const PORT = process.env.PORT || 3058;
 
 const server = http.createServer(async (req, res) => {
     const bodyToSend = "gopal23"
-    if(req.url === '/route1', req.method === "GET") {
+    if(req.url === '/route1' && req.method === "GET") {
         res.writeHead(200, {"Content-Type": "application/json"});
         // res.writeHead(200, {'Content-Length': Buffer.byteLength(bodyToSend), 'Content-Type': 'text/plain'});
         res.write(`Hi, you've hit route1 with ${req.method} method`);
         res.end();
     }
+    if(req.url === '/route2') {
+        const res23 = await getRequestBody(req);
+        console.log(res23);
+        res.write(JSON.stringify(res23));
+        res.end();
+    }
 });
+
+function getRequestBody(reqObj) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = "";            
+            reqObj.on("data", (chunk) => { body += chunk.toString(); });            
+            reqObj.on("end", () => { resolve(body); });
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
 
 server.listen(PORT, () => {
     console.log(`server23 started on port ${PORT}`);
